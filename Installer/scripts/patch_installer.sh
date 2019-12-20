555eb2b23cdfff6425f72823f9cf5d190624aaa0
osy <osy86@users.noreply.github.com>
2019-12-19 18:54:13 -0800

    installer: fixed permission issues in Catalina
    In Catalina, file system access is granted per-location. Removable devices
    are one such location that requires explicit permission to access.
    
    Due to a bug (feature?), even if you grant Installer.app permission to
    access removable devices, that permission does not extend to the Shell
    scripts the Installer runs as part of the install.
    
    However, due to another bug (feature?) in the permission system, the access
    to external drives is limited ONLY to the root directory of the drive. This
    means we cannot do a listing on the root directory because we lack the
    permission, but we CAN read/write anything beyond that including all files
    and subdirectories of the drive. Now some may say this makes the security
    feature completely useless because as long as you can guess/brute force the
    names of the files/directories on the drive, then you can access it without
    any extra permissions. But do not tell Apple this or we would need another
    workaround.
    
    So instead of finding InstallInfo.plist automatically, we now hard code a
    list of all possible paths to InstallInfo.plist (currently this only
    includes "Install macOS Mojave.app" and "Install macOS Catalina.app" but now
    we have the unfortunate task of adding to this list with every new OSX
    release). Side note: the modding of InstallInfo.plist is itself another
    bug/feature because technically the OSX installer is signed and all pkgs
    that is included must be signed. But somehow, if you include your pkg under
    "Additional Installers", it does not have to be signed. Technically this is
    not a security issue because you would need admin priviledges to even change
    the OSX installer app, but then why bother blocking unsigned packages in the
    other sections? Anyways if Apple "fixes" this then we no longer get an easy
    installer patch so our time here is limited anyways and maybe we do not have
    to hard code every future OSX installer path anyways.
    
    tl;dr: this fixes #96 and related issues. You no longer need to turn off SIP.

--- Installer/scripts/patch_installer.sh
+++ Installer/scripts/patch_installer.sh
@@ -2,8 +2,30 @@
 
 set -e
 
+# In Catalina, we can no longer read external drives so `find $3` no longer 
+# works instead we hard code all possible InstallInfo.plist paths and see which
+# one exists. This means with every new OSX release, we have to modify this.
+SUPPORTED_PATHS="
+Install macOS Mojave.app/Contents/SharedSupport/InstallInfo.plist
+Install macOS Catalina.app/Contents/SharedSupport/InstallInfo.plist
+"
+INSTALL_INFO=
+
+while read line; do
+    path="$3/$line"
+    if [ ! -z "$line" -a -f "$path" ]; then
+        echo "Found install info at $path"
+        INSTALL_INFO="$path"
+    fi
+done <<< "$SUPPORTED_PATHS"
+
+if [ -z "$INSTALL_INFO" ]; then
+    echo "Cannot find InstallInfo.plist, $3 may not be a valid OSX installer volume"
+    echo "Otherwise, this script does not recognize the OSX installer version, here's the known list: $SUPPORTED_PATHS"
+    exit 1
+fi
+
 PLIST_BUDDY="/usr/libexec/PlistBuddy"
-INSTALL_INFO=`find "$3" -name 'InstallInfo.plist'`
 SHARED_SUPPORT=`dirname "$INSTALL_INFO"`
 
 patched=`$PLIST_BUDDY -c "Print :Additional\ Installers" "$INSTALL_INFO" | grep "HaCMini.pkg" || true`
