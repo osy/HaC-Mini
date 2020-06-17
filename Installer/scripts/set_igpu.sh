cf2c5734c58e372455392db01c5960c0a3006a0d
osy <osy86@users.noreply.github.com>
2020-06-17 13:02:16 -0700

    installer: use AMD decoder with Intel codec option
    * Intel encoder needed for Sidecar support
    * AMD encoder seems broken (lots of artifacts)
    * Intel decoder is broken for H.264 content at 120 FPS

--- Installer/scripts/set_igpu.sh
+++ Installer/scripts/set_igpu.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-rm -f /Library/Preferences/com.apple.AppleGVA.plist
-printf "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 " > "$INSTALLER_TEMP/boot-args-gpu.txt"
+printf "%s " "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 -radcodec" > "$INSTALLER_TEMP/boot-args-gpu.txt"
