041a1cff1083c8fd629f63c1d494fc813e90675d
osy <osy86@users.noreply.github.com>
2020-06-23 19:51:40 -0700

    installer: add IGPU offline rendering
    OOB update for WhateverGreen

--- Installer/scripts/set_igpu.sh
+++ Installer/scripts/set_igpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf "%s " "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 -radcodec" > "$INSTALLER_TEMP/boot-args-gpu.txt"
+printf "%s " "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 -radcodec igfxmetal=1" > "$INSTALLER_TEMP/boot-args-gpu.txt"
