d69fb1932bf68349a2303c14fd61b7a142feed2f
osy <osy86@users.noreply.github.com>
2020-08-14 10:45:00 -0700

    Revert "installer: use AMD decoder with Intel codec option"
    This reverts commit cf2c5734c58e372455392db01c5960c0a3006a0d.

--- Installer/scripts/set_igpu.sh
+++ Installer/scripts/set_igpu.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-printf "%s " "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 -radcodec igfxmetal=1" > "$INSTALLER_TEMP/boot-args-gpu.txt"
+rm -f /Library/Preferences/com.apple.AppleGVA.plist
+printf "%s " "shikigva=32 shiki-id=Mac-BE088AF8C5EB4FA2 igfxmetal=1" > "$INSTALLER_TEMP/boot-args-gpu.txt"
