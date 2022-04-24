73d08dcdd68138c16ef60f017dbe09e1be6753c2
goodbest <lovegoodbest@gmail.com>
2022-04-24 19:59:07 +0800

    Udpate 202204 (#8)
    * Revert "fix boot stuck on Monterey 12.1+ for brcm wifi card"
    
    This reverts commit 240c0f51746ec6a0fd67f9dcfe7e33f67f47ff83.
    
    * Use boot-arg instead of DSDT to "fix boot stuck on Monterey 12.1+ for brcm wifi card"
    
    DSDT will cause unexpected behavior on my test machine
    
    * add brcmfx boot-args for Monterey 12.1+ for brcm card
    
    * extend the delay time longer
    
    * bug fix: fix verbose and debug mode cannot be effective at the same time

--- Installer/scripts/set_brcmfx.sh
+++ Installer/scripts/set_brcmfx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf "%s " "brcmfx-delay=2000" > "$INSTALLER_TEMP/boot-args-brcmfx.txt"
+printf "%s " "brcmfx-delay=5000" > "$INSTALLER_TEMP/boot-args-brcmfx.txt"
--- Installer/scripts/set_debug.sh
+++ Installer/scripts/set_debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf "%s " "keepsyms=1 debug=0x146 watchdog=0 -liludbgall" > "$INSTALLER_TEMP/boot-args-verbose.txt"
+printf "%s " "keepsyms=1 debug=0x146 watchdog=0 -liludbgall" > "$INSTALLER_TEMP/boot-args-debug.txt"
